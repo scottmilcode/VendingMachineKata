976dc8b4d95dfe0def20c09244015badd99545cd
Scott Miller <scottmil@gmail.com>
2017-01-02 17:39:19 -0600

    Changed test such that test order was no longer relevant, added additional test cases

--- src/test/java/DisplayTest.java
+++ src/test/java/DisplayTest.java
@@ -12,31 +12,43 @@ import static junit.framework.TestCase.assertEquals;
 public class DisplayTest {
     Display display; //Display object used for testing
 
-    @Before
-    public void setUp() {
-        display = new Display(true);
-    }
-
-    //Default message test cases
+   //Default message test cases
     @Test
     public void afterInitializationWithTrueGetMessageReturnsInsertCoin() {
+        display = new Display(true);
         assertEquals("INSERT COIN", display.getMessage());
     }
 
     @Test
-    public void whenChangeNotAvailableGetMessageReturnsExactChangeOnly() {
+    public void afterInitializationWithFalseGetMessageReturnsInsertCoin() {
+        display = new Display(false);
+        assertEquals("EXACT CHANGE ONLY", display.getMessage());
+    }
+
+    @Test
+    public void afterInitializationWithFalseAndSetChangeToTrueGetMessageReturnsInsertCoin() {
+        display = new Display(false);
+        display.setChangeAvailable(true);
+        assertEquals("INSERT COIN", display.getMessage());
+    }
+
+    @Test
+    public void afterInitializationWithTrueAndSetChangeToFalseGetMessageReturnsInsertCoin() {
+        display = new Display(true);
         display.setChangeAvailable(false);
         assertEquals("EXACT CHANGE ONLY", display.getMessage());
     }
 
     @Test
     public void whenChangeAvailableGetMessageReturnsExactChangeOnly() {
+        display = new Display(false);
         display.setChangeAvailable(true);
         assertEquals("INSERT COIN", display.getMessage());
     }
 
     @Test
     public void whenProductVendedAndChangeStillAvailableGetMessageReturnsThankYouFollowedByInsertCoin() {
+        display = new Display(true);
         display.setProductVended();
         assertEquals("THANK YOU", display.getMessage());
         assertEquals("INSERT COIN", display.getMessage()); //this check to ensure that the message returns to appropriate default
@@ -44,6 +56,7 @@ public class DisplayTest {
 
     @Test
     public void whenProductSoldOutAndChangeStillAvailableGetMessageReturnsProductSoldOutFollowedByInsertCoin() {
+        display = new Display(true);
         display.setProductSoldOut();
         assertEquals("SOLD OUT", display.getMessage());
         assertEquals("INSERT COIN", display.getMessage());
@@ -51,6 +64,7 @@ public class DisplayTest {
 
     @Test
     public void whenProductSelectedGetMessageReturnsPriceAndProductPriceFollowedByInsertCoin(){
+        display = new Display(true);
         display.setProductSelected(0.65);
         assertEquals("PRICE $0.65", display.getMessage());
         assertEquals("INSERT COIN", display.getMessage());
@@ -58,9 +72,22 @@ public class DisplayTest {
 
     @Test
     public void whenChangeInsertedGetMessageReturnsTotalAmountAvailableForPurchase(){
+        display = new Display(true);
         display.coinInserted(0.05);
         assertEquals("$0.05", display.getMessage());
         display.coinInserted(0.10);
         assertEquals("$0.15", display.getMessage());
+    }
+
+    @Test
+    public void whenChangeInsertedGetMessageReturnsTotalAmountAvailableForPurchaseThenAfterPurchaseThankYouThenInsertCoin() {
+        display = new Display(true);
+        display.coinInserted(0.05);
+        assertEquals("$0.05", display.getMessage());
+        display.coinInserted(0.10);
+        assertEquals("$0.15", display.getMessage());
+        display.setProductVended();
+        assertEquals("THANK YOU", display.getMessage());
+        assertEquals("INSERT COIN", display.getMessage());
     }
 }
