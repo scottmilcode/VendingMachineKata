00117b7b7a8689d5ca3e864102cc0229b9153490
Scott Miller <scottmil@gmail.com>
2017-01-05 09:23:59 -0600

    Updated ChangeManagerTest to use Ints to represent currency

--- src/test/java/ChangeManagerTest.java
+++ src/test/java/ChangeManagerTest.java
@@ -4,7 +4,6 @@
 ///////////////////////////////
 package VendingMachine;
 import org.junit.Test;
-
 import java.util.ArrayList;
 import static junit.framework.TestCase.assertEquals;
 
@@ -12,39 +11,48 @@ import static junit.framework.TestCase.assertEquals;
 public class ChangeManagerTest {
 
     //All tests for correct change assume that the lowest number of coins based on available
-    //change will be returned. Other options would have
+    //change will be returned.
 
     @Test
     public void whenNoChangeIsRequiredMakeChangeReturnsNoChange(){
         ChangeManager changeManager = new ChangeManager(1, 1, 1);
-        assertEquals(0, changeManager.makeChange(0.50, 0.50).size());
+        ArrayList<Integer> actualReturnedChange = new ArrayList<Integer>();
+
+        changeManager.makeChange(50, 50, actualReturnedChange);
+        assertEquals(0, actualReturnedChange.size());
     }
 
     @Test
     public void whenOneQuarterShouldBeReturnedMakeChangeReturnsOneQuarter(){
         ChangeManager changeManager = new ChangeManager(1, 1, 1);
-        ArrayList<Double> returnedChange = new ArrayList<Double>();
-        returnedChange.add(0.25);
+        ArrayList<Integer> actualReturnedChange = new ArrayList<Integer>();
+        ArrayList<Integer> properReturnedChange = new ArrayList<Integer>();
+        properReturnedChange.add(25);
 
-        assertEquals(returnedChange, changeManager.makeChange(0.50, 0.75));
+        changeManager.makeChange(50, 75, actualReturnedChange);
+        assertEquals(properReturnedChange, actualReturnedChange);
     }
 
     @Test
     public void whenOneDimeShouldBeReturnedMakeChangeReturnsOneDime(){
         ChangeManager changeManager = new ChangeManager(1, 1, 1);
-        ArrayList<Double> returnedChange = new ArrayList<Double>();
-        returnedChange.add(0.10);
+        ArrayList<Integer> actualReturnedChange = new ArrayList<Integer>();
+        ArrayList<Integer> properReturnedChange = new ArrayList<Integer>();
+        properReturnedChange.add(10);
 
-        assertEquals(returnedChange, changeManager.makeChange(0.50, 0.65));
+        changeManager.makeChange(50, 60, actualReturnedChange);
+        assertEquals(properReturnedChange, actualReturnedChange);
     }
 
     @Test
     public void whenOneNickelShouldBeReturnedMakeChangeReturnsOneNickel(){
         ChangeManager changeManager = new ChangeManager(1, 1, 1);
-        ArrayList<Double> returnedChange = new ArrayList<Double>();
-        returnedChange.add(0.05);
+        ArrayList<Integer> actualReturnedChange = new ArrayList<Integer>();
+        ArrayList<Integer> properReturnedChange = new ArrayList<Integer>();
+        properReturnedChange.add(5);
 
-        assertEquals(returnedChange, changeManager.makeChange(0.50, 0.55));
+        changeManager.makeChange(50, 55, actualReturnedChange);
+        assertEquals(properReturnedChange, actualReturnedChange);
     }
 
     //Test that check for for proper coins being returned when the optimal coin is not available
@@ -53,32 +61,40 @@ public class ChangeManagerTest {
     @Test
     public void whenTwentyFiveCentsShouldBeReturnedAndNoQuarterAvailableMakeChangeReturnsTwoDimesAndOneNickel(){
         ChangeManager changeManager = new ChangeManager(1, 2, 0);
-        ArrayList<Double> returnedChange = new ArrayList<Double>();
-        returnedChange.add(0.10);
-        returnedChange.add(0.10);
-        returnedChange.add(0.05);
+        ArrayList<Integer> actualReturnedChange = new ArrayList<Integer>();
+        ArrayList<Integer> properReturnedChange = new ArrayList<Integer>();
+        properReturnedChange.add(10);
+        properReturnedChange.add(10);
+        properReturnedChange.add(5);
 
-        assertEquals(returnedChange, changeManager.makeChange(0.50, 0.75));
+        changeManager.makeChange(50, 75, actualReturnedChange);
+        assertEquals(properReturnedChange, actualReturnedChange);
     }
 
     @Test
     public void whenTwentyFiveCentsShouldBeReturnedAndNoQuarterOrDimeAvailableMakeChangeReturnsFiveNickels(){
         ChangeManager changeManager = new ChangeManager(5, 0, 0);
-        ArrayList<Double> returnedChange = new ArrayList<Double>();
-        returnedChange.add(0.10);
-        returnedChange.add(0.10);
-        returnedChange.add(0.05);
+        ArrayList<Integer> actualReturnedChange = new ArrayList<Integer>();
+        ArrayList<Integer> properReturnedChange = new ArrayList<Integer>();
+        properReturnedChange.add(5);
+        properReturnedChange.add(5);
+        properReturnedChange.add(5);
+        properReturnedChange.add(5);
+        properReturnedChange.add(5);
 
-        assertEquals(returnedChange, changeManager.makeChange(0.50, 0.75));
+        changeManager.makeChange(50, 75, actualReturnedChange);
+        assertEquals(properReturnedChange, actualReturnedChange);
     }
 
     @Test
     public void whenTenCentsShouldBeReturnedAndNoDimeAvailableMakeChangeReturnsTwoNickels(){
         ChangeManager changeManager = new ChangeManager(2, 0, 1);
-        ArrayList<Double> returnedChange = new ArrayList<Double>();
-        returnedChange.add(0.05);
-        returnedChange.add(0.05);
+        ArrayList<Integer> actualReturnedChange = new ArrayList<Integer>();
+        ArrayList<Integer> properReturnedChange = new ArrayList<Integer>();
+        properReturnedChange.add(05);
+        properReturnedChange.add(05);
 
-        assertEquals(returnedChange, changeManager.makeChange(0.50, 0.60));
+        changeManager.makeChange(50, 60, actualReturnedChange);
+        assertEquals(properReturnedChange, actualReturnedChange);
     }
 }
